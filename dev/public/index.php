100f76201682193aa29aaf0742d1feeb36397a29
Thierry Lagasse <epagneul@dogstudio.be>
2016-08-02 13:51:41 +0200

    Create debug route to retrieve NHC locations and actions

--- dev/public/index.php
+++ dev/public/index.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 defined('ROOT_DIR') || define('ROOT_DIR', realpath(__DIR__ . '/../'));
 require_once ROOT_DIR . '/vendor/autoload.php';
 
+$config = require_once ROOT_DIR . '/datas/config.php';
 $app = new Silex\Application();
+$niko = new \Niko\Niko($config['nhc']['address'] ?: '10.0.0.30', $config['nhc']['port'] ?: 8000);
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello ' . $app->escape($name);
+$app->get('/debug', function () use ($app, $niko) {
+    return '<pre>' . print_r($niko->getLocationActions(), true);
 });
 
 $app->run();
