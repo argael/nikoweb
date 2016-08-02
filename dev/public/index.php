0fe3649f0475ec7bd12f3de4f27d7148af4e30b3
Thierry Lagasse <epagneul@dogstudio.be>
2016-08-02 11:46:11 +0200

    Install and use silex

--- dev/public/index.php
+++ dev/public/index.php
@@ -1,3 +1,11 @@
 <?php 
+defined('ROOT_DIR') || define('ROOT_DIR', realpath(__DIR__ . '/../'));
+require_once ROOT_DIR . '/vendor/autoload.php';
 
-    echo 'Test: ', date('c');
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+});
+
+$app->run();
