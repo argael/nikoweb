3e4a76e13dfff7f234c0006387a72afc949f7293
Thierry Lagasse <epagneul@dogstudio.be>
2016-08-02 20:21:17 +0200

    Prepare a new class to manage action

---
+++ dev/src/Niko/NikoAction.php
@@ -0,0 +1,45 @@
+<?php
+namespace Niko;
+
+use Niko\Niko;
+use Niko\Exception\NikoActionException;
+
+class NikoAction
+{
+    const ACTION_STORY = 0;
+    const ACTION_SWITCH = 1;
+    const ACTION_SHUTTER = 4;
+
+    public $id;
+    public $name;
+    public $type;
+    public $location;
+    public $value;
+    public $values;
+
+    public function __construct($propeties=[])
+    {
+        if ($propeties) {
+            if (!isset($propeties['id']) || !isset($propeties['name'])) {
+                throw new NikoActionException('Invalid properties');
+            }
+
+            $this->id = $propeties['id'];
+            $this->id = $propeties['id'];
+
+            $this->type = isset($propeties['type'])
+                ? $propeties['type']
+                : self::ACTION_SWITCH;
+
+            $this->location = isset($propeties['location'])
+                ? $propeties['location']
+                : 0;
+
+            $this->value = isset($propeties['value1'])
+                ? $propeties['value1']
+                : 0;
+
+            $this->values = [];
+        }
+    }
+}
